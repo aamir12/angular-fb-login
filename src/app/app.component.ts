3e10369eacab4398a0c626a910382eaade23fe78
aamir12 <aamirkhan8878@gmail.com>
2021-01-03 12:05:25 +0530

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FBLoginService } from './fblogin.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'socialLogin';
+export class AppComponent implements OnInit {
+  constructor(private fbLogin: FBLoginService) {}
+  ngOnInit() {
+    this.fbLogin.init();
+  }
+
+  loggedIn = false;
+  user: any = {};
+  async signInWithFB() {
+    const data = await this.fbLogin.login();
+    if (data) {
+      this.loggedIn = true;
+      console.log(data);
+      this.user = data;
+    }
+  }
 }
